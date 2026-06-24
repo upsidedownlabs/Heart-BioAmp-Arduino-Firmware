6011be7e365da76b4336f070890db1c4f212f8f0
Ankit-dhillon <ankitdhillon295@gmail.com>
2026-06-24 11:43:44 +0530

    fixed comments and folder name and formatted document

--- 02_ECG_Filter/02_ECG_Filter.ino
+++ 02_ECG_Filter/02_ECG_Filter.ino
@@ -41,28 +41,28 @@
 #define INPUT_PIN A0
 
 void setup() {
-	// Initialize serial communication
-	Serial.begin(BAUD_RATE);
+  // Initialize serial communication
+  Serial.begin(BAUD_RATE);
 }
 
 void loop() {
-	// Calculate elapsed time
-	static unsigned long past = 0;
-	unsigned long present = micros();
-	unsigned long interval = present - past;
-	past = present;
+  // Calculate elapsed time
+  static unsigned long past = 0;
+  unsigned long present = micros();
+  unsigned long interval = present - past;
+  past = present;
 
-	// Run timer
-	static long timer = 0;
-	timer -= interval;
+  // Run timer
+  static long timer = 0;
+  timer -= interval;
 
-	// Sample
-	if(timer < 0){
-		timer += 1000000 / SAMPLE_RATE;
-		float sensor_value = analogRead(INPUT_PIN);
-		float signal = ECGFilter(sensor_value);
-		Serial.println(signal);
-	}
+  // Sample
+  if (timer < 0) {
+    timer += 1000000 / SAMPLE_RATE;
+    float sensor_value = analogRead(INPUT_PIN);
+    float signal = ECGFilter(sensor_value);
+    Serial.println(signal);
+  }
 }
 
 // Band-Pass Butterworth IIR digital filter, generated using filter_gen.py.
@@ -71,34 +71,33 @@ void loop() {
 // Reference:
 // https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.butter.html
 // https://courses.ideate.cmu.edu/16-223/f2020/Arduino/FilterDemos/filter_gen.py
-float ECGFilter(float input)
-{
+float ECGFilter(float input) {
   float output = input;
   {
-    static float z1, z2; // filter section state
-    float x = output - 0.70682283*z1 - 0.15621030*z2;
-    output = 0.28064917*x + 0.56129834*z1 + 0.28064917*z2;
+    static float z1, z2;  // filter section state
+    float x = output - 0.70682283 * z1 - 0.15621030 * z2;
+    output = 0.28064917 * x + 0.56129834 * z1 + 0.28064917 * z2;
     z2 = z1;
     z1 = x;
   }
   {
-    static float z1, z2; // filter section state
-    float x = output - 0.95028224*z1 - 0.54073140*z2;
-    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
+    static float z1, z2;  // filter section state
+    float x = output - 0.95028224 * z1 - 0.54073140 * z2;
+    output = 1.00000000 * x + 2.00000000 * z1 + 1.00000000 * z2;
     z2 = z1;
     z1 = x;
   }
   {
-    static float z1, z2; // filter section state
-    float x = output - -1.95360385*z1 - 0.95423412*z2;
-    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    static float z1, z2;  // filter section state
+    float x = output - -1.95360385 * z1 - 0.95423412 * z2;
+    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
     z2 = z1;
     z1 = x;
   }
   {
-    static float z1, z2; // filter section state
-    float x = output - -1.98048558*z1 - 0.98111344*z2;
-    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    static float z1, z2;  // filter section state
+    float x = output - -1.98048558 * z1 - 0.98111344 * z2;
+    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
     z2 = z1;
     z1 = x;
   }
